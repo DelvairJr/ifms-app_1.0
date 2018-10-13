03033dfada59b18e8b6821dd5fff5c8e0b711bef
DelvairJr <delvair.ifms@gmail.com>
2018-10-12 21:03:46 -0400

    Impl. autenticação em Express.

--- backend_express/src/config/routes.js
+++ backend_express/src/config/routes.js
@@ -1,44 +1,29 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function (server) {
-    //URL base
-    const router = express.Router()
-    server.use('/api', router)
-
-    //Rota de usuario
-    const Usuario = require('../api/usuario/usuarioService')
-    Usuario.register(router, '/admin')
+    /*
+     * Rotas protegidas por Token JWT 
+     */
+    //defini a rota a partir da função Router do Express
+    const protectedApi = express.Router()
+    //aplica a rota ao servidor que possui a rota /api
+    server.use('/api', protectedApi)
+    //Aplica o filtro de autenticação
+    protectedApi.use(auth)
 
     //Rota de professor
     const Professor = require('../api/professor/professorService')
-    Professor.register(router, '/professores')
+    Professor.register(protectedApi, '/professores')
 
-    //Rota eventos
-    const Evento = require('../api/evento/eventoService')
-    Evento.register(router, '/eventos')
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
 
-    //Rota provas
-    const Prova = require('../api/calendarioProva/provasService')
-    Prova.register(router, '/provas')
-
-    //Rota horario de permanência
-    const Permanencia = require('../api/horarioPermanencia/permanenciaService')
-    Permanencia.register(router, '/horario-de-permanencia')
-
-    //Rota Contatos
-    const Contatos = require('../api/contato/contatosService')
-    Contatos.register(router, '/contatos')
-
-    //Rota Cursos
-    const Cursos = require('../api/cursos/cursoService')
-    Cursos.register(router, '/cursos')
-
-    //Rota editais
-    const Edital = require('../api/editais/editalService')
-    Edital.register(router, '/editais')
-
-    //Rota regulamento
-    const Regulamento = require('../api/regulamentos/regulamentoService')
-    Regulamento.register(router, '/regulamentos')
-
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
